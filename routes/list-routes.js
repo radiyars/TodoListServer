a9753b7bc2c34535cf58026f52140f800bf09280
radiyars <radiyars@yandex.ru>
2023-04-25 19:33:01 +0300

    vercel.json edited for enabling CORS. 3

--- routes/list-routes.js
+++ routes/list-routes.js
@@ -6,11 +6,11 @@ const router = express.Router()
 
 
 
-router.get('api/lists', getLists)
-router.get('api/lists/:id', getList)
-router.delete('api/lists/:id', deleteList)
-router.post('api/lists', postList)
-router.patch('api/lists/:id', updateList)
+router.get('/api/lists', getLists)
+router.get('/api/lists/:id', getList)
+router.delete('/api/lists/:id', deleteList)
+router.post('/api/lists', postList)
+router.patch('/api/lists/:id', updateList)
 
 
 module.exports = router
